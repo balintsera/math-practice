3e94a04a162d4a70793442ab7666f6b79ac65a38
Bálint Séra <balint.sera@redsteedstudios.com>
2019-02-09 14:54:59 +0100

    Fix deploy, add charset meta

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 BUCKET=luca-unicorns
-aws s3 cp . s3://$BUCKET --recursive --exclude '.git/*' --exclude 'deploy.sh' --exclude 'cloudformation.yml' --profile default
+aws s3 cp . s3://$BUCKET --recursive --exclude '.git/*' --exclude 'deploy.sh' --exclude 'cloudformation.yml' --profile default --acl public-read
